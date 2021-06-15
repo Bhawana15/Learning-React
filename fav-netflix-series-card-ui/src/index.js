41a6fad431b75cd00270f12bd09fb5f332f3fcc8
Bhawana15 <bhawanatiwari888@gmail.com>
2021-06-15 18:45:59 +0530

    Updated fav netflix series, used map()

--- fav-netflix-series-card-ui/src/index.js
+++ fav-netflix-series-card-ui/src/index.js
@@ -7,36 +7,46 @@ import Sdata from "./Sdata";
 ReactDOM.render(
     <>
         <h1 className="heading_style"> List of my Favorite Netflix Series </h1>
-        <Card 
-            imgsrc = { Sdata[0].imgsrc }
-            title = { Sdata[0].title }
-            sname = { Sdata[0].sname }
-            link = { Sdata[0].link }
-        />
-        <Card 
+
+        {Sdata.map((val) => {
+                return (
+                    <Card
+                        imgsrc={val.imgsrc}
+                        title={val.title}
+                        sname={val.sname}
+                        link={val.link}
+                    />
+                );
+            }
+        )}
+
+    </>
+    , document.getElementById('root')
+);
+
+/*
+<Card
             imgsrc = { Sdata[1].imgsrc }
             title = { Sdata[1].title }
             sname = { Sdata[1].sname }
             link = { Sdata[1].link }
         />
-        <Card 
+        <Card
         imgsrc = { Sdata[2].imgsrc }
             title = { Sdata[2].title }
             sname = { Sdata[2].sname }
             link = { Sdata[2].link }
         />
-        <Card 
+        <Card
         imgsrc = { Sdata[3].imgsrc }
             title = { Sdata[3].title }
             sname = { Sdata[3].sname }
-            link = { Sdata[3].link } 
+            link = { Sdata[3].link }
         />
-        <Card 
+        <Card
         imgsrc = { Sdata[4].imgsrc }
             title = { Sdata[4].title }
             sname = { Sdata[4].sname }
-            link = { Sdata[4].link } 
+            link = { Sdata[4].link }
         />
-    </>
-    , document.getElementById('root')
-);
+        */
